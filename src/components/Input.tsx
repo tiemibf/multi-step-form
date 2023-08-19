6673ec163254bea4c7496a44c17be7b64754b75b
Tiemi Fernandes <tiemi.bfnagazawa@hotmail.com>
2023-08-18 21:07:13 -0400

    chore: set tailwinds theme colors

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -8,14 +8,14 @@ interface IInputProps {
 export const Input = ({ placeholder, name, label, value }: IInputProps) => {
     return (
         <div className="mb-6">
-            <label htmlFor={name} className="text-[#022959 font-normal text-sm">
+            <label htmlFor={name} className="text-ms-denim font-normal text-sm">
                 {label}
             </label>
             <input
                 placeholder={placeholder}
                 value={value}
                 name={name}
-                className="border border-[#D6D9E6] rounded-lg h-12 w-full px-4 py-[10px] font-medium mt-1"
+                className="border border-ms-light-grey rounded-lg h-12 w-full px-4 py-[10px] font-medium mt-1"
             />
         </div>
     );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,21 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'ms-bg-blue': '#EFF5FF',
+        'ms-sky-blue': '#BEE2FD',
+        'ms-light-blue': '#ABBCFF',
+        'ms-purple': '#483EFF',
+        'ms-denim': '#022959',
+        'ms-lightest-grey': '#F8F9FF',
+        'ms-light-grey': '#D6D9E6',
+        'ms-grey': '#9699AA',
+        'ms-orange': '#FFAF7E',
+        'ms-pink': '#F9818E',
+        'ms-red': '#EE374A',
+      }
     },
-  },
-  plugins: [],
+    plugins: [],
+  }
 }
 export default config
