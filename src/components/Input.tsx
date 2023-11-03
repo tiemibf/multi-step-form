1d97d8d521aca443d3d0f012140b532c6d53b57a
Tiemi Fernandes <tiemi.bfnagazawa@hotmail.com>
2023-11-03 08:59:43 -0400

    refactor: implements onChange behavior on Inputs

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,11 +1,20 @@
+import { ChangeEventHandler } from "react";
+
 interface IInputProps {
     placeholder: string;
     value?: string;
     name: string;
     label: string;
+    onChange: ChangeEventHandler<HTMLInputElement>;
 }
 
-export const Input = ({ placeholder, name, label, value }: IInputProps) => {
+export const Input = ({
+    placeholder,
+    name,
+    label,
+    value,
+    onChange
+}: IInputProps) => {
     return (
         <div className="mb-6">
             <label htmlFor={name} className="text-ms-denim font-normal text-sm">
@@ -15,6 +24,7 @@ export const Input = ({ placeholder, name, label, value }: IInputProps) => {
                 placeholder={placeholder}
                 value={value}
                 name={name}
+                onChange={onChange}
                 className="border border-ms-light-grey rounded-lg h-12 w-full px-4 py-[10px] font-medium mt-1"
             />
         </div>
--- src/pages/PersonalInfo.tsx
+++ src/pages/PersonalInfo.tsx
@@ -1,7 +1,19 @@
 import { Input } from "@/components/Input";
 import { PageTitle } from "@/components/PageTitle";
+import { useState } from "react";
 
 export const PersonalInfoPage = () => {
+    const [state, setState] = useState({
+        name: "",
+        email: "",
+        phone: ""
+    });
+
+    const handleChange = (event: any) => {
+        const value = event.target.value;
+        setState({ ...state, [event.target.name]: value });
+    };
+
     return (
         <div>
             <div className="flex-1 w-full mb-8">
@@ -11,15 +23,25 @@ export const PersonalInfoPage = () => {
                 />
             </div>
 
-            <Input label="Name" name="name" placeholder="e.g. Stephen King" />
+            <Input
+                label="Name"
+                name="name"
+                value={state.name}
+                onChange={handleChange}
+                placeholder="e.g. Stephen King"
+            />
             <Input
                 label="Email Address"
                 name="email"
+                value={state.email}
+                onChange={handleChange}
                 placeholder="e.g. stephenking@lorem.com"
             />
             <Input
                 label="Phone Number"
                 name="phone"
+                value={state.phone}
+                onChange={handleChange}
                 placeholder="e.g. +1 234 567 890"
             />
         </div>
