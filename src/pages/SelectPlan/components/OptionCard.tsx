65a89fe7b56b6e63a2578e3ac03ea21366e74a14
Tiemi Fernandes <tiemi.bfnagazawa@hotmail.com>
2023-11-20 17:38:48 -0400

    feat: adds validation on select plan page

--- src/pages/SelectPlan/components/OptionCard.tsx
+++ src/pages/SelectPlan/components/OptionCard.tsx
@@ -6,9 +6,10 @@ import { IPlanOptions } from "../planOptions";
 
 interface IOptionCardProps {
     plan: IPlanOptions;
-    benefits?: string;
     isSelected: boolean;
+    error: boolean;
     onClick: () => void;
+    benefits?: string;
 }
 
 const lookUpTable = {
@@ -16,13 +17,13 @@ const lookUpTable = {
     yearly: "yr"
 };
 
-export const OptionCard = ({ plan: { icon, color, title, price }, isSelected, benefits, onClick }: IOptionCardProps) => {
+export const OptionCard = ({ plan: { icon, color, title, price }, isSelected, benefits, error, onClick }: IOptionCardProps) => {
     const { formData } = useContext(FormContext);
     const { paymentFrequency } = formData;
     const priceLabel = lookUpTable[paymentFrequency];
 
     return (
-        <SelectableCard isSelected={isSelected} onClick={onClick} className="w-[138px] h-40 px-4 py-5">
+        <SelectableCard isSelected={isSelected} error={error} onClick={onClick} className="w-[138px] h-40 px-4 py-5">
             <div className="h-full flex flex-col justify-between">
                 <Icon color={color} icon={icon} />
 
