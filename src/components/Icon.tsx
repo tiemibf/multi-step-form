520e097ec0674769e43d780f6cf09f0c044f06fc
Tiemi Fernandes <tiemi.bfnagazawa@hotmail.com>
2024-02-12 10:47:59 -0400

    fix: fixes icons position

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -4,18 +4,5 @@ interface IIconProps {
 }
 
 export const Icon = ({ color, icon }: IIconProps) => {
-    return (
-        <>
-            <svg
-                width="40"
-                height="40"
-                viewBox="0 0 40 40"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-                <circle cx="16.5" cy="16.5" r="16.5" fill={color} />
-                <g transform="translate(10,6)">{icon}</g>
-            </svg>
-        </>
-    );
+    return <div className={`rounded-full w-10 h-10 flex items-center justify-center bg-[${color}]`}>{icon}</div>;
 };
