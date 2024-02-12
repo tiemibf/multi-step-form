87ac58410b239fd081e6089dedeebe2907ca332a
Tiemi Fernandes <tiemi.bfnagazawa@hotmail.com>
2024-02-12 13:37:24 -0400

    fix: icon's background color

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -4,5 +4,9 @@ interface IIconProps {
 }
 
 export const Icon = ({ color, icon }: IIconProps) => {
-    return <div className={`rounded-full w-10 h-10 flex items-center justify-center bg-[${color}]`}>{icon}</div>;
+    return (
+        <div className="rounded-full w-10 h-10 flex items-center justify-center" style={{ backgroundColor: color }}>
+            {icon}
+        </div>
+    );
 };
